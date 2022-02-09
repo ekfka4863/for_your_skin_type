265c20ff767e3991f50edd08097f674c26b198be
ekfka4863 <ekfka4863@gmail.com>
2022-02-10 01:19:33 +0900

    [Add] update code for sign up logic

--- src/pages/LoginSignup.js
+++ src/pages/LoginSignup.js
@@ -1,6 +1,5 @@
 import React,{ useState, useRef } from "react";
 // import React,{ useState, useRef, useEffect } from "react";
-// import { Link } from "react-router-dom";
 
 
 // 공통 컴포넌트 임포트 
@@ -17,23 +16,9 @@ import SignUpSection from "../components/SignUpSection";
 // styling 
 import "../styles/src/LoginSignup.scss";
 
-// img 
-// import cart from "../assets/img/tablet/cart.png";
-// import myPage from "../assets/img/tablet/myPage.png";
-// import go_to_test_btn from "../assets/img/laptop/go_to_test_btn.png";
-
-
-
-
-// export let logInControler = false;
-// let sessionLatestId = "";
 
 export default function LoginSignup() {
-  let loggedIn = useRef(false);
-  // const [loggedIn, setLoggedIn] = useState(true);
-  // const [loggedIn, setLoggedIn] = useState(false);
-
-  // let authenticatedId = useRef("");
+  // let loggedIn = useRef(false);
 
   const [tapbar, setTapbar] = useState(1);
 
@@ -43,29 +28,10 @@ export default function LoginSignup() {
 
 
 
-  // useEffect(() => {
-  //   console.log("loggedIn, logInControler => ", loggedIn.current, logInControler);
-  //   console.log("localStorage.getItem('authenticatedId') => ", localStorage.getItem("authenticatedId"));
-  // }, [loggedIn.current, logInControler, localStorage.getItem("authenticatedId")]);
-  
-
-
-
-
-
   return(
     <>
       <Header />
       <div id="LoginSignup_wrap">
-        {
-          (localStorage.getItem("authenticatedId") !== "loginFail" && localStorage.getItem("authenticatedId") !== null)
-        ? 
-          <>
-            {/* 확인하기!!! */}
-            {/* <LoggedInPage />  */}
-            <div>로그인 완료!</div>
-          </>
-        :  
           <div className="LoginSignup_bind">
 
           <ul className="taps">
@@ -75,20 +41,15 @@ export default function LoginSignup() {
 
           {/* 로그인박스 */}
           <div className={ tapbar === 1 ? 'Login_box' : 'Content_none'} onClick={() => {toggle(1)}}>
-            {/* <LogInSection /> */}
-            <div>로그인 박스에유~!</div>
+            <LogInSection />
           </div>
           
 
           {/* 회원가입박스 */}
           <div className={ tapbar === 2? 'Signup_box' : 'Content_none'} onClick={() => {toggle(2)}}>
-            {/* <SignUpSection /> */}
-            <div>회원가입 박스에유~!</div>
+            <SignUpSection />
           </div>
-
         </div>
-        }
-        
       </div>
       <Footer />
     </>
